832703de78fca15a3f19cf48b937b34035196cd9
jmLascano <jm.lascano2@gmail.com>
2023-11-27 16:53:04 -0500

    Make basic importable via making py file w/ funcs

---
+++ basic.py
@@ -0,0 +1,48 @@
+# Functions for importing, fully explained in basic.ipynb
+
+import re
+import json
+
+def fetchData():
+    path = "cards.json"
+    with open(path, "r", encoding="utf8") as f:
+        raw = f.read()
+        data = json.loads(raw)
+
+    return data
+
+def cleanData():
+    colorDict = {
+        'W': [], # white
+        'U': [], # blue
+        'B': [], # black
+        'R': [], # red
+        'G': [], # green
+        'colorless': [] # everything else
+    }
+
+    for i in data:
+
+        # Skip joke cards
+        if i['set_type'] == 'funny':
+            pass
+
+        # Clean \n and sub out cardname 
+        if 'oracle_text' in i.keys():
+            text = i['oracle_text'] 
+            text = re.sub("\n", "", text)
+            text = re.sub(re.escape(i['name']), "CARDNAME", text)
+            i['oracle_text'] = text
+        else:
+            pass 
+
+        # Put into color buckets
+        colors = i['color_identity']
+        if [] == colors:
+            colorless.append(i)
+            pass
+        
+        for color in colors:
+            colorDict[color].append(i)
+        
+    return colorDict
